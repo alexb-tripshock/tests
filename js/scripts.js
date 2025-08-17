a5a8be08f30dcfbe376dc83829bd322e9d5eed4b
alexb-tripshock <alexb@tripshock.com>
2025-08-17 14:28:47 -0400

    Menu object for footer, css and html modifications

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,14 @@ const menus = {
         "Contacts": "contacts.html",
         "About": "about.html",
     },
-    footer: ["Privacy Policy", "Terms of Service", "Help"],
+    footer: {
+        "О проекте": "about.html",
+        "Контакты": "contacts.html",
+        "Политика конфиденциальности": "privacy.html",
+        "Условия использования": "terms.html",
+        "Глоссарий": "glossary.html",
+        "Помощь": "help.html"
+    },
     menuMaker: function(x) {
         return Object.entries(x).map(([key, value]) => `<a href="${value}">${key}</a>`).join(" | ");
     },
